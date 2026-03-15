dfdc47792d5529ed48e2561a022eca1c38f69014
Anjali Mallur <anjmallur@gmail.com>
2026-03-15 12:52:46 +0530

    tried streamlit

--- chatbot.py
+++ chatbot.py
@@ -1,68 +1,22 @@
 from google import genai
+import streamlit as st
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+st.title("Anjali's Chatbot")
+
 api_key = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-user_input = input("anjali: ")
+user_input = st.text_input("anjali: ")
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
     contents=user_input
 )
 
-print("bot:", response.text)
+st.write("bot:", response.text)
 
-'''import google.generativeai as genai
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-api_key = os.getenv("GEMINI_API_KEY")
-
-genai.configure(api_key=api_key)
-
-model = genai.GenerativeModel(model_name="gemini-3-flash-preview")
-
-def add_numbers(a: float, b: float):
-    return a + b
-
-tools = [
-    {
-        "functionDeclarations": [
-            {
-                "name": "add_numbers",
-                "description": "Add two numbers together",
-                "parameters": {
-                    "type": "OBJECT",
-                    "properties": {
-                        "a": {"type": "NUMBER"},
-                        "b": {"type": "NUMBER"}
-                    },
-                    "required": ["a", "b"]
-                }
-            }
-        ]
-    }
-]
-
-
-chat_history = []
-
-while True:
-    user_input = input("You: ")
-
-    chat_history.append({"role": "user", "parts": [user_input]})
-
-    response = model.generate_content(
-        contents=chat_history,
-        config={
-            "tools": tools
-        }
-    )
-    print(response)'''
