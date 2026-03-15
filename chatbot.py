7734bdb7d5b1076cafe1ff016e06d290e742c2b5
Anjali Mallur <anjmallur@gmail.com>
2026-03-15 13:19:22 +0530

    colours are in!

--- chatbot.py
+++ chatbot.py
@@ -3,20 +3,26 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+st.set_page_config(
+    page_title="Anjali du Pre Frontal Cortex",
+    page_icon="🧘‍♂️"
+)
+st.title("Anjali du Pre Frontal Cortex")
+st.header("This is a simple chatbot built using Gemini API and Streamlit.")
+st.caption("This is fun, but I will be adding more features soon. Stay tuned!")
 
-st.title("Anjali's Chatbot")
+load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-user_input = st.text_input("anjali: ")
+user_input = st.text_input("you: ")
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
     contents=user_input
 )
 
-st.write("bot:", response.text)
+st.write("my brain:", response.text)
 
